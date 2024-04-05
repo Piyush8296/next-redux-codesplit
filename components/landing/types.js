3f687983077cdfde84b909afdd63f4b185a29c33
Piyush Bansal <piyush.bansal@cars24.com>
2024-04-05 13:16:17 +0530

    removed create types

--- components/landing/types.js
+++ components/landing/types.js
@@ -1,13 +1,10 @@
-import { createTypes } from "../../redux/create-types";
+const prefix = 'test/';
 
-export default createTypes(
-    `
-        FETCH_DATA
-        FETCH_DATA_SUCCESS
-        FETCH_DATA_FAILURE
-        UPDATE_SSR_STATUS
-    `,
-    {
-        prefix: "test/"
-    }
-);
+const Types = {
+  FETCH_DATA: `${prefix}FETCH_DATA`,
+  FETCH_DATA_SUCCESS: `${prefix}FETCH_DATA_SUCCESS`,
+  FETCH_DATA_FAILURE: `${prefix}FETCH_DATA_FAILURE`,
+  UPDATE_SSR_STATUS: `${prefix}UPDATE_SSR_STATUS`,
+};
+
+export default Types;
